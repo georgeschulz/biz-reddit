115208c88f05225149d33a343107dfadf85e4fd0
97812180+georgeschulz@users.noreply.github.com <97812180+georgeschulz@users.noreply.github.com>
2022-05-23 22:05:05 -0400

    Create Post presentational component

---
+++ src/app/components/post/post.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Post = () => {
+    return (
+        <div className='post'>
+            <div className='vote-column'>
+                <p>1001</p>
+            </div>
+            <div className='content-column'>
+                <h2>Jeff Bezos Annouces Retirement</h2>
+                <p></p>
+            </div>
+        </div>
+    )
+}
+
+export default Post;
