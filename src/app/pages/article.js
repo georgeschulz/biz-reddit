707520e8683eb787652a3f5a373a373cf97d4d25
97812180+georgeschulz@users.noreply.github.com <97812180+georgeschulz@users.noreply.github.com>
2022-05-24 18:58:07 -0400

    Add destructuring off article

--- src/app/pages/article.js
+++ src/app/pages/article.js
@@ -1,10 +1,36 @@
 import React from "react";
+import { useParams } from 'react-router-dom'
 
 function Article(props) {
+    const { posts } = props;
+    const { id } = useParams();
+    const {
+        header,
+        votes,
+        text,
+        author,
+        img
+    } = posts[id];
+    
+    function htmlDecode(input) {
+        const paragraphs = input.split("\n");
+        const parsedParagraphs = [];
+
+        paragraphs.forEach(paragraph => {
+            var doc = new DOMParser().parseFromString(paragraph, "text/html");
+            parsedParagraphs.push(doc.documentElement.textContent)
+        })
+
+        return parsedParagraphs.map(p => <p>{p}</p>);
+    }
+
     return (
-        <main>
-            <p>Article</p>
-        </main>
+        <article>
+            <h1>{header}</h1>
+            <p className="byline">Written by: {author}</p>
+            <img src={img} className="article-image" />
+            {htmlDecode(text)}
+        </article>
     )
 }
 
