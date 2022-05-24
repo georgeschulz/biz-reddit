8f460cdef13be2531364211c4d21520c45291011
97812180+georgeschulz@users.noreply.github.com <97812180+georgeschulz@users.noreply.github.com>
2022-05-23 22:04:46 -0400

    Write html for desktop feed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
 import React from 'react';
 import './App.css';
+import logo from './assets/logo.png'
+import bookmark from './assets/bookmark.png'
+import filter from './assets/filter.png'
+import Post from './app/components/post/post';
 
 function App() {
   return (
-    <div className="App">       
+    <div className="App">
+      <header>
+        <img src={logo} className="logo" />
+        <h1>Business Reddit</h1>
+      </header>
+      <main>
+        <aside>
+          <div className="nav-group">
+            <h3><img src={bookmark} /> Bookmarks</h3>
+            <ul className='bookmark-list'>
+              <li>Big Tech</li>
+              <li>Finance</li>
+              <li>Online Business</li>
+              <li>Fundraising</li>
+              <li>Startups</li>
+              <li>Small Business</li>
+            </ul>
+          </div>
+          <div className='nav-group'>
+            <h3><img src={filter} /> Filters</h3>
+            <ul className='bookmark-list'>
+              <li>OP Only</li>
+              <li>Funny</li>
+              <li>Memes</li>
+            </ul>
+          </div>
+        </aside>
+        <div className='feed'>
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+        </div>
+      </main>
     </div>
   );
 }
