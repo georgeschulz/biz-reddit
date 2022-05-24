6e0793098f9b8b09e30b2e8c905182d62258325c
97812180+georgeschulz@users.noreply.github.com <97812180+georgeschulz@users.noreply.github.com>
2022-05-24 18:40:23 -0400

    Implement router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import './App.css';
 import logo from './assets/logo.png'
-import { BrowserRouter as Router } from 'react-router-dom';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
 import Feed from './app/pages/feed';
 import placeholder from './assets/placeholder.jpeg'
+import Article from './app/pages/article';
 
 function App() {
   const posts = [
@@ -24,7 +30,7 @@ function App() {
       img: placeholder
     },
     {
-      id: 2,
+      id: 3,
       header: "Jeff Bezos Annouces Retirement",
       votes: 1000,
       text: "&quot;In surprising news, Amazon founder and CEO Jeff Bezos announced he will be stepping down as the chief executive officer in a press release. Bezos, an extremely wealthy man, will “transition to the role of Executive Chair” later this year, and Andy Jassy will take the reins as CEO and new subject of internet socialist ire. Jassy is currently the chief executive of Amazon’s cloud computing division and part-owner of the Seattle Krakens.\n\nBezos’ puzzling retirement comes out of the blue. In a letter to Amazon employees, he says he intends “to focus my energies and attention on new products and early initiatives” and lauds “invention” as key to Amazon’s success. He notably does not explain why he is stepping down as CEO, leaving the internet to come up with their own guesses; hilarity predictably ensues as Twitter imagines Bezos’ new career path.\n\nBezos’ puzzling retirement comes out of the blue. In a letter to Amazon employees, he says he intends “to focus my energies and attention on new products and early initiatives” and lauds “invention” as key to Amazon’s success. He notably does not explain why he is stepping down as CEO, leaving the internet to come up with their own guesses; hilarity predictably ensues as Twitter imagines Bezos’ new career path.\n\nBezos’ puzzling retirement comes out of the blue. In a letter to Amazon employees, he says he intends “to focus my energies and attention on new products and early initiatives” and lauds “invention” as key to Amazon’s success. He notably does not explain why he is stepping down as CEO, leaving the internet to come up with their own guesses; hilarity predictably ensues as Twitter imagines Bezos’ new career path.\n\nRelated: MacKenzie Bezos To Give Away Half Her $37 Billion Fortune\n&quot;;",
@@ -32,7 +38,7 @@ function App() {
       img: placeholder
     },
     {
-      id: 2,
+      id: 4,
       header: "Jeff Bezos Annouces Retirement",
       votes: 1000,
       text: "&quot;In surprising news, Amazon founder and CEO Jeff Bezos announced he will be stepping down as the chief executive officer in a press release. Bezos, an extremely wealthy man, will “transition to the role of Executive Chair” later this year, and Andy Jassy will take the reins as CEO and new subject of internet socialist ire. Jassy is currently the chief executive of Amazon’s cloud computing division and part-owner of the Seattle Krakens.\n\nBezos’ puzzling retirement comes out of the blue. In a letter to Amazon employees, he says he intends “to focus my energies and attention on new products and early initiatives” and lauds “invention” as key to Amazon’s success. He notably does not explain why he is stepping down as CEO, leaving the internet to come up with their own guesses; hilarity predictably ensues as Twitter imagines Bezos’ new career path.\n\nBezos’ puzzling retirement comes out of the blue. In a letter to Amazon employees, he says he intends “to focus my energies and attention on new products and early initiatives” and lauds “invention” as key to Amazon’s success. He notably does not explain why he is stepping down as CEO, leaving the internet to come up with their own guesses; hilarity predictably ensues as Twitter imagines Bezos’ new career path.\n\nBezos’ puzzling retirement comes out of the blue. In a letter to Amazon employees, he says he intends “to focus my energies and attention on new products and early initiatives” and lauds “invention” as key to Amazon’s success. He notably does not explain why he is stepping down as CEO, leaving the internet to come up with their own guesses; hilarity predictably ensues as Twitter imagines Bezos’ new career path.\n\nRelated: MacKenzie Bezos To Give Away Half Her $37 Billion Fortune\n&quot;;",
@@ -40,7 +46,7 @@ function App() {
       img: placeholder
     },
     {
-      id: 2,
+      id: 5,
       header: "Jeff Bezos Annouces Retirement",
       votes: 1000,
       text: "&quot;In surprising news, Amazon founder and CEO Jeff Bezos announced he will be stepping down as the chief executive officer in a press release. Bezos, an extremely wealthy man, will “transition to the role of Executive Chair” later this year, and Andy Jassy will take the reins as CEO and new subject of internet socialist ire. Jassy is currently the chief executive of Amazon’s cloud computing division and part-owner of the Seattle Krakens.\n\nBezos’ puzzling retirement comes out of the blue. In a letter to Amazon employees, he says he intends “to focus my energies and attention on new products and early initiatives” and lauds “invention” as key to Amazon’s success. He notably does not explain why he is stepping down as CEO, leaving the internet to come up with their own guesses; hilarity predictably ensues as Twitter imagines Bezos’ new career path.\n\nBezos’ puzzling retirement comes out of the blue. In a letter to Amazon employees, he says he intends “to focus my energies and attention on new products and early initiatives” and lauds “invention” as key to Amazon’s success. He notably does not explain why he is stepping down as CEO, leaving the internet to come up with their own guesses; hilarity predictably ensues as Twitter imagines Bezos’ new career path.\n\nBezos’ puzzling retirement comes out of the blue. In a letter to Amazon employees, he says he intends “to focus my energies and attention on new products and early initiatives” and lauds “invention” as key to Amazon’s success. He notably does not explain why he is stepping down as CEO, leaving the internet to come up with their own guesses; hilarity predictably ensues as Twitter imagines Bezos’ new career path.\n\nRelated: MacKenzie Bezos To Give Away Half Her $37 Billion Fortune\n&quot;;",
@@ -56,7 +62,14 @@ function App() {
           <img src={logo} className="logo" />
           <h1>Business Reddit</h1>
         </header>
-        <Feed posts={posts} />
+        <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Feed posts={posts}></Feed>} />
+          <Route path="/post" element={<Article />} />
+        </Routes>
+        </BrowserRouter>
+        
+        
       </div>
     </div>
   );
---
+++ src/app/pages/article.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Article(props) {
+    return (
+        <main>
+            <p>Article</p>
+        </main>
+    )
+}
+
+export default Article;
